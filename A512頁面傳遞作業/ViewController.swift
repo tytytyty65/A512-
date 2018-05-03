308b9dcb0e08c3a4b67c53e1f895aecf5752122b
范博勝 <tytytyty65@icloud.com>
2018-05-03 21:09:12 +0800

    Initial Commit

---
+++ A512頁面傳遞作業/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A512頁面傳遞作業
+//
+//  Created by 范博勝 on 2018/5/3.
+//  Copyright © 2018年 范博勝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
